94f2d2d8fd3aa478d7442549b9376220a0f894cb
Christian <christiantenz7@gmail.com>
2023-09-21 14:13:42 -0500

    try-catch added

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,49 +1,73 @@
-import {pool} from '../db.js'
+import { pool } from '../db.js';
 
-export const getTasks = async (req,res) =>{
-    const [result] = await pool.query("SELECT * FROM tbl_tasks ORDER BY createAt ASC")
-    res.json(result)
-}
+export const getTasks = async (req, res) => {
+  try {
+    const [result] = await pool.query("SELECT * FROM tbl_tasks ORDER BY createAt ASC");
+    res.json(result);
+  } catch (error) {
+    console.error("Error in getTasks:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-export const getTask = async(req,res) =>{
+export const getTask = async (req, res) => {
+  try {
+    const [result] = await pool.query("SELECT * FROM tbl_tasks WHERE id = ?", [req.params.id]);
 
-    const [result] = await pool.query("SELECT * FROM tbl_tasks WHERE id = ?", [req.params.id])
-
-    if(result.length === 0){
-        return res.status(404).json({
-            message : "Task not found"
-        })
+    if (result.length === 0) {
+      return res.status(404).json({
+        message: "Task not found"
+      });
     }
 
-    res.json(result[0])
+    res.json(result[0]);
+  } catch (error) {
+    console.error("Error in getTask:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-}
+export const createTask = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const [result] = await pool.query("INSERT INTO tbl_tasks(title, description) VALUES(?, ?)", [title, description]);
 
-export const createTask = async (req,res) =>{
-    const {title,description} = req.body;
-    const [result] = await pool.query("INSERT INTO tbl_tasks(title,description) VALUES(?,?)", [title,description])
-    
-   res.json({
-    id: result.insertId,
-    title,
-    description
-   })
-}
+    res.json({
+      id: result.insertId,
+      title,
+      description
+    });
+  } catch (error) {
+    console.error("Error in createTask:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-export const updateTask = (req,res) =>{
-    res.send("creando tarea")
+export const updateTask = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const [result] = await pool.query("UPDATE tbl_tasks SET ? WHERE id = ?", [req.body, req.params.id]);
 
-}
+    res.json(result);
+  } catch (error) {
+    console.error("Error in updateTask:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-export const deleteTask = async(req,res) =>{
+export const deleteTask = async (req, res) => {
+  try {
     const [result] = await pool.query("DELETE from tbl_tasks WHERE id = ?", [req.params.id]);
 
-    if(result.affectedRows === 0){
-        return res.status(404).json({
-            message : "Task not found"
-        })
+    if (result.affectedRows === 0) {
+      return res.status(404).json({
+        message: "Task not found"
+      });
     }
-    return res.sendStatus(204)
-}
 
-
+    return res.sendStatus(204);
+  } catch (error) {
+    console.error("Error in deleteTask:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
